d7a95113abdbf22956eb7f9a286714aa28443034
martinrodriguez19 <tinchorodriguez918@gmail.com>
2025-04-28 16:44:59 -0300

    mongo

--- src/app/lib/mongoose.ts
+++ src/app/lib/mongoose.ts
@@ -13,6 +13,11 @@ export const connectToDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL,{
             dbName:'ELCACIQUE',
+            bufferCommands: false,
+            serverSelectionTimeoutMS: 10000,  // Aumenta a 10 segundos
+            socketTimeoutMS: 30000,           // Aumenta a 30 segundos
+            connectTimeoutMS: 10000,          // Aumenta a 10 segundos
+            maxPoolSize: 10,                  // Limita el pool de conexiones
         })
         isConnected = true 
         console.log('MongoDB connected')
